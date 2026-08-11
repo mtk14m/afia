a5829d92ed455ae6a62de8549901d4bad2460663
mtk14n <mtk14minou@gmail.com>
2026-08-11 03:36:12 +0200

    fix(gateway): remove deprecated middleware.RealIP (IP spoofing vulnerability)

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -27,7 +27,6 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
